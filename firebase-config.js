608c4b394914b21ae590678031ccc05a58638e92
jasontal1 <jason@jasontal.com>
2026-09-15 11:18:07 -0400

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,14 @@
+// Paste your Firebase web app settings here.
+// Firebase console > Project settings (gear icon) > General > Your apps > Web app > "SDK setup and configuration" > Config.
+// These values are not secret; it is normal for them to be in a public GitHub repo.
+// Access to your data is controlled by the rules in firestore.rules.
+ 
+export const firebaseConfig = {
+  apiKey: "AIzaSyD3JtUhk4wziVhNWs_HRM2c4_NpeJ1I23k",
+  authDomain: "cleanup-tal.firebaseapp.com",
+  projectId: "cleanup-tal",
+  storageBucket: "cleanup-tal.firebasestorage.app",
+  messagingSenderId: "20428168516",
+  appId: "1:20428168516:web:d92c38a6b37d1cc03ccb11",
+  measurementId: "G-1RRQFDPK6V"
+};
